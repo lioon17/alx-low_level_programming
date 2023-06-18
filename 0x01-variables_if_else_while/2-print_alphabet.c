7bbd287728f778a7437995f0550539627972c4bc
lioon17 <99320650+lioon17@users.noreply.github.com>
2023-06-18 19:10:44 +0300

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 /**
  * main - Prints the alphabet.
  *
@@ -10,25 +7,13 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
 	int i;
 
 	for (i = 0; i < 26; i++)
 	{
-		putchar(alp[i]);
+	    putchar(alp[i]);
 	}
-
 	putchar('\n');
 	return (0);
-=======
-        char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-        int i;
-        for (i = 0; i < 26; i++)
-        {
-                putchar(alp[i]);
-        }
-        putchar('\n');
-        return (0);
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 }
