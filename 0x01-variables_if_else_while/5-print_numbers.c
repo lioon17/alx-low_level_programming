4e5c817ba35ab94ede14510d61c7905f044ea6e9
lioon17 <99320650+lioon17@users.noreply.github.com>
2023-06-18 23:32:00 +0300

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,8 +1,6 @@
+5-print_numbers.c
 #include <stdio.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -10,23 +8,11 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	int i;
-
 	for (i = 0; i < 10; i++)
 	{
 		printf("%d", i);
 	}
-
 	putchar('\n');
 	return (0);
-=======
-        int i;
-        for (i = 0; i < 10; i++)
-        {
-                printf("%d", i);
-        }
-        putchar('\n');
-        return (0);
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 }
