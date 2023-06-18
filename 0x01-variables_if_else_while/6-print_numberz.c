77cf710378edbf63e09f88eeaf5fbf8b603b0d2b
lioon17 <99320650+lioon17@users.noreply.github.com>
2023-06-18 23:36:24 +0300

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,8 +1,5 @@
 #include <stdio.h>
-<<<<<<< HEAD
 
-=======
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 /**
  * main - Prints numbers from 0 to 9.
  *
@@ -10,23 +7,11 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	int i;
-
 	for (i = 48; i < 58; i++)
 	{
 		putchar(i);
 	}
-
 	putchar('\n');
 	return (0);
-=======
-        int i;
-        for (i = 48; i < 58; i++)
-        {
-                putchar(i);
-        }
-        putchar('\n');
-        return (0);
->>>>>>> 5080a5b23dc211cdd545acd217e4cdcae798d2c1
 }
