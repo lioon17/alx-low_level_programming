026a03cd0c1a651610fccc4a9fd885cd12353db0
lioon17 <99320650+lioon17@users.noreply.github.com>
2023-06-19 13:53:10 +0300

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,3 +1,4 @@
+4-print_alphabt.c
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,7 +14,7 @@ int main(void)
 
 	for (i = 97; i < 123; i++)
 	{
-	if (i != 101 && i != 113)
+		if (i != 101 && i != 113)
 		{
 			putchar(i);
 		}
